f74c31c0bd113024ae07da20b44b84d9d635bef8
Kevin Daysal <kdaysal123@gmail.com>
2022-09-04 23:54:26 -0400

    Updated telephone input to add hyphens (-) for responsive formatting

--- js/script.js
+++ js/script.js
@@ -1,74 +1,88 @@
-(function() {
-    let form = document.querySelector('.contact-form');
-    let emailInput = document.querySelector('#contact-email');
-    let telephoneInput = document.querySelector('#telephone');
-    
-    function showErrorMessage(input, message) {
-      let container = input.parentElement; // The .input-wrapper
-      
-      // Remove an existing error
-      let error = container.querySelector('.error-message');
-      if (error) {
-        container.removeChild(error);
-      }
-      
-      // Now add the error, if the message is not empty
-      if (message) {
-        let error = document.createElement('div');
-        error.classList.add('error-message');
-        error.innerText = message;
-        container.appendChild(error);
-      }
+(function () {
+  let form = document.querySelector('.contact-form');
+  let emailInput = document.querySelector('#contact-email');
+  let telephoneInput = document.querySelector('#telephone');
+
+  function showErrorMessage(input, message) {
+    let container = input.parentElement; // The .input-wrapper
+
+    // Remove an existing error
+    let error = container.querySelector('.error-message');
+    if (error) {
+      container.removeChild(error);
     }
-    
-    function validateEmail() {
-      let value = emailInput.value;
-      
-      if (!value) {
-        showErrorMessage(emailInput, 'E-mail is a required field.');
-        return false;
-      }
-      
-      if (value.indexOf('@') === -1) {
-        showErrorMessage(emailInput, 'Invalid email format - please keep typing :)');
-        return false;
-      }
-      
-      showErrorMessage(emailInput, null);
-      return true;
+
+    // Now add the error, if the message is not empty
+    if (message) {
+      let error = document.createElement('div');
+      error.classList.add('error-message');
+      error.innerText = message;
+      container.appendChild(error);
     }
-    
-    function validatePhoneNum() {
-      let value = telephoneInput.value;
-      
-      if (!value) {
-        showErrorMessage(telephoneInput, 'Telephone number is a required field.');
-        return false;
-      }
-      
-      if (value.length < 10) {
-        showErrorMessage(telephoneInput, 'Telephone number must be at least 10 digits long.');
-        return false;
-      }
-      
-      showErrorMessage(telephoneInput, null);
-      return true;
+  }
+
+  function validateEmail() {
+    let value = emailInput.value;
+
+    if (!value) {
+      showErrorMessage(emailInput, 'E-mail is a required field.');
+      return false;
     }
-    
-    function validateForm() {
-      let isValidEmail = validateEmail();
-      let isValidTelephone = validatePhoneNum();
-      return isValidEmail && isValidTelephone;
+
+    if (value.indexOf('@') === -1) {
+      showErrorMessage(emailInput, 'Invalid email format - please keep typing :)');
+      return false;
     }
-    
-    form.addEventListener('submit', (e) => {
-      e.preventDefault(); // Do not submit to the server
-      if (validateForm()) {
-        alert('Success!');
-      }
-    });
-    
-    emailInput.addEventListener('input', validateEmail);
-    telephoneInput.addEventListener('input', validatePhoneNum);
-  })();
+
+    showErrorMessage(emailInput, null);
+    return true;
+  }
+
+  function validatePhoneNum() {
+    let value = telephoneInput.value;
+
+
+
+
+    //add hyphen (-) to format telephone number correctly
+    if (value.length === 3) {
+      value += '-';
+      telephoneInput.value = value;
+      console.log(`telephone value now: ${value}`)
+    }
+
+    //add hyphen (-) to format telephone number correctly
+    if (value.length === 7) {
+      value += '-';
+      telephoneInput.value = value;
+      console.log(`telephone value now: ${value}`)
+    }
+
+    if (value.length < 12) {
+      showErrorMessage(telephoneInput, 'Telephone number must be at least 10 digits long.');
+      return false;
+    }
+
+
+    //if no validation flags, then remove the error message
+    showErrorMessage(telephoneInput, null);
+    return true;
+  }
+
+  function validateForm() {
+    let isValidEmail = validateEmail();
+    let isValidTelephone = validatePhoneNum();
+    return isValidEmail && isValidTelephone;
+  }
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault(); // Do not submit to the server
+    if (validateForm()) {
+      alert('Success!');
+    }
+  });
+
+  emailInput.addEventListener('input', validateEmail);
+  telephoneInput.addEventListener('input', validatePhoneNum);
+})();
 
